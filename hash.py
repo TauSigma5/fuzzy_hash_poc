d63cb29c3e307bdcb075a70d4ab43e88ba39f790
Aaron Zhang <tausigma5@pop-os.localdomain>
2021-01-19 20:57:25 -0800

    Make the use case more clear and add more comments

--- hash.py
+++ hash.py
@@ -2,21 +2,23 @@ import os
 from PIL import Image
 import imagehash
 
-
+# Imagine a database of fuzzy hashes of login sites of commonly used sites
+# with their proper URLs attached to them.
 hashedImage = imagehash.average_hash(Image.open("real_site.png"))
-print(hashedImage)
+real_site_url = "account.protonmail.com"
 
-# Screenshotted from phishing link website
-check_image = imagehash.average_hash(Image.open("image2.png"))
-print(check_image)
+# Screenshotted from phishing link website and its associated link
+check_image = imagehash.average_hash(Image.open("image3.png"))
+suspicious_url = "account.protonemail.com"
 
 # Add a check to see if the link is from the appropriate website
-
 # If url is from the official site, don't show as phishing
 # however, if url is not correct, yet the site still looks
 # very similar, then mark as phishing.
-if check_image == hashedImage:
-    print("Phishing Link")
+if suspicious_url != real_site_url:
+    if check_image == hashedImage:
+        print("Phishing Link")
+    else:
+        print("Safe")
 else:
     print("Safe")
-
